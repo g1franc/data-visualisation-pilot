c74095811a6a8e2d7271b1c8b597db29182ab688
brule.daniel@gmail.com <brule.daniel@gmail.com>
2016-12-01 19:01:09 +0100

    python to generate the json per org + some json example +

--- Data cleaning Visualisation 3/Scripts/GenerateJSONforOrg.py
+++ Data cleaning Visualisation 3/Scripts/GenerateJSONforOrg.py
@@ -1,4 +1,4 @@
-#Scripts/GenerateJSONfororg.py Output/NbLinkOrg.csv Output/OrgInformation.csv
+#Scripts/GenerateJSONfororg.py Output/NbLinkOrgLevel1.csv Output/OrgInformation.csv
 
 
 import sys
@@ -9,27 +9,24 @@ class JNode:
 		self.index = index
 		self.links = links
 		self.label = line[1]
-		self.NumberProjectParticipation = line[2]
-		self.score = 7 #same bubble size for all
+		self.score = line[2] 
 		self.id = index
 	def toJSON(self):
-		return json.dumps({'index':self.index,'links':self.links,'label':self.label,'NumberProjectParticipation':self.NumberProjectParticipation,'score':self.score,'id':self.index},separators=(',', ':'),indent=4)
+		return json.dumps({'index':self.index,'links':self.links,'label':self.label,'score':self.score,'id':self.index},separators=(',', ':'),indent=4)
 
 class JLink:
-    def __init__(self, source, target, weight):
-        self.source = source
-        self.target = target
-        self.weight = weight
-
-    def toJSON(self):
-        return json.dumps({'source':self.source,'target':self.target,'weight':self.weight},separators=(',', ':'),indent=4)
+	def __init__(self, source, target, weight):
+		self.source = source
+		self.target = target
+		self.weight = weight
+	def toJSON(self):
+		return json.dumps({'source':self.source,'target':self.target,'weight':self.weight},separators=(',', ':'),indent=4)
 
 
 sepChar = ';'
 
-orgList = [];
-nodesList = [];
-linksList = [];
+orgDictionary = {};
+
 
 FileLink = sys.argv[1]
 FileOrg = sys.argv[2]
@@ -41,42 +38,66 @@ linesOrg = [line.rstrip('\n') for line in open(FileOrg)]
 
 for i in range(1, len(linesOrg)):
 	lineList = linesOrg[i].split(sepChar)
-	orgList.append(lineList[1])
-	nodesList.append(JNode(len(orgList)-1,[],lineList))
+	orgDictionary[lineList[1]] = JNode(i,[],lineList)
 
-
-for i in range(1, len(linesLink)):
-	lineList = linesLink[i].split(sepChar)
-	if(lineList[1] != lineList[2]):
-		#node
-		framework = lineList[4]
-		currentOrgIndex = orgList.index(lineList[1])
-		cNode = nodesList[currentOrgIndex]
-		cNode.links.append(orgList.index(lineList[2]))
-		linksList.append(JLink(cNode.index, orgList.index(lineList[2]), float(lineList[4])))
 
 #write JSON output file
 def WriteJSON(nameOutputFile, nodelist, linksList):
-	outputName = nameOutputFile
-	outputFile = open(outputName, 'w')
-	outputFile.write('{\n')
-	outputFile.write('"nodes": [\n');
-	for i in range(len(nodelist)-1):
-		outputFile.write(nodelist[i].toJSON())
-		outputFile.write(',\n')
-	outputFile.write(nodelist[len(nodelist)-1].toJSON())
-	outputFile.write('\n')
-	outputFile.write('],\n')
-	outputFile.write('"links":[\n')
-	for i in range(len(linksList)-1):
-		outputFile.write(linksList[i].toJSON())
-		outputFile.write(',\n')
-	outputFile.write(linksList[len(linksList)-1].toJSON())
-	outputFile.write(']\n')
-	outputFile.write('}')
+	try:
+		outputName = nameOutputFile
+		outputFile = open(outputName, 'w')
+		outputFile.write('{\n')
+		outputFile.write('"nodes": [\n');
+		for i in range(len(nodelist)-1):
+			outputFile.write(nodelist[i].toJSON())
+			outputFile.write(',\n')
+		outputFile.write(nodelist[len(nodelist)-1].toJSON())
+		outputFile.write('\n')
+		outputFile.write('],\n')
+		outputFile.write('"links":[\n')
+		for i in range(len(linksList)-1):
+			outputFile.write(linksList[i].toJSON())
+			outputFile.write(',\n')
+		outputFile.write(linksList[len(linksList)-1].toJSON())
+		outputFile.write(']\n')
+		outputFile.write('}')
+	except:
+		print nameOutputFile
 
 
+name = "";
+NodeDic = {};
+linksList = [];
+for i in range(1, len(linesLink)):
+	lineList = linesLink[i].split(sepChar)
+	curName = lineList[4]
+	if(name != curName and len(NodeDic) != 0):
+		NodeList = [];
+		for key, value in NodeDic.iteritems():
+			NodeList.append(value)
+		WriteJSON("outputJS/" + name + ".json", NodeList, linksList)
+		NodeDic = {};
+		linksList = [];
+	name = curName
+	
+	org1Name = lineList[1]
+	org2Name = lineList[2]
+	if (not org1Name in NodeDic)  :
+		NodeDic[org1Name] = orgDictionary[org1Name]
+	if (not org2Name in NodeDic):
+		NodeDic[org2Name] = orgDictionary[org2Name]
+	
+	org1Index = NodeDic[org1Name].id
+	org2Index = NodeDic[org2Name].id
+	
+	NodeDic[org1Name].links.append(org2Index)
+	linksList.append(JLink(org1Index, org2Index, float(lineList[3])))
 
-WriteJSON("outputJS/org.json",nodesList, linksList)
 
+NodeList = [];
+for key, value in NodeDic.iteritems():
+	NodeList.append(value)
+WriteJSON("outputJS/" + name + ".json", NodeList, linksList)
+NodeDic = {};
+linksList = [];
 
