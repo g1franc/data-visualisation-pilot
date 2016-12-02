f8b37395fc94e2df88cc07fd91c2aabf7cf09f7d
brule.daniel@gmail.com <brule.daniel@gmail.com>
2016-12-02 12:56:49 +0100

    prepare data for the chord diagram

---
+++ Data cleaning Visualisation 4 - Chord/Scripts/DataPrep-Link.R
@@ -0,0 +1,134 @@
+
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------
+# I. LOAD DATASETS ############################################################################################################################
+# ---------------------------------------------------------------------------------------------------------------------------------------------
+
+#set current directory
+setwd("C:/Users/bruled/Documents/Pwc Project/2 - Project/DataVisualisation/Visualisation/1 - Cordis/data-visualisation-pilot/Data cleaning Visualisation 4 - Chord")
+
+### Load datasets
+# FP6: https://data.europa.eu/euodp/data/dataset/cordisfp6projects
+# FP7: https://data.europa.eu/euodp/data/dataset/cordisfp7projects
+# H2020: https://data.europa.eu/euodp/data/dataset/cordisH2020projects
+Dataset_FP6Organizations = read.csv("Input/cordis-fp6organizations.csv", header=TRUE, sep=";", stringsAsFactors=FALSE, comment.char="")
+
+Dataset_FP7Organizations = read.csv("Input/cordis-fp7organizations.csv", header=TRUE, sep=";", stringsAsFactors=FALSE, comment.char="")
+
+Dataset_H2020Organizations = read.csv("Input/cordis-h2020organizations.csv", header=TRUE, sep=";", stringsAsFactors=FALSE, comment.char="")
+
+Dataset_Countries = read.csv("Input/Countries.csv", header=TRUE, sep=";", stringsAsFactors=FALSE, comment.char="")
+listCountries <- sort(Dataset_Countries$euCode)
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------
+# II. FUNCTIONS LINK ##########################################################################################################################
+# ---------------------------------------------------------------------------------------------------------------------------------------------
+
+
+func_size <- function(vector){
+  return(length(unlist(vector)))
+}
+
+func_order <- function(vector){
+  return (as.character(sort(unlist(vector))))
+}
+
+#take a vector (e.g. [A,B,C]) and return all the possible pair of value (e.g. [[A, B],[A, C],[B, C]]) 
+func_createPair <- function(vector){
+  if (length(vector) > 1) {
+    return (t(combn(unlist(vector), 2)))
+  }
+  else
+  {
+    return (c())
+  }
+}
+
+#transform a list [a,b,c,d,e,f] in a frame with 2 columns [[a,b,c],[d,e,f]]
+func_createFrame <- function(vector){
+  return(data.frame(matrix(unlist(vector), ncol = 2, byrow=FALSE)))
+}
+
+# build a dataset containing all the interactions between the different countries  
+func_BuildLink <- function(frameworkContract, dataset) {
+  # frameworkContract <- "FP6"
+  # dataset <- Dataset_FP6Organizations
+  
+  dataset <- merge(dataset, Dataset_Countries, by.x=c("country"), by.y=c("euCode"), all.x=TRUE)
+
+  dataset <- dataset[dataset$EU28 == TRUE, ]
+  dataset <- dataset[!is.na(dataset$EU28),]
+  
+  dataset <- subset(dataset, select=-c(projectAcronym, role, id, name.x, shortName, activityType, endOfParticipation, ecContribution, street, city, postCode,
+                                        organizationUrl, contactType, contactTitle, contactFirstNames, contactLastNames, contactFunction, contactTelephoneNumber, contactFaxNumber,
+                                        contactEmail, FP6participantCountries,FP7participantCountries,FP8participantCountries, isoCode,EU28,Schengen, country))
+  dataset <- plyr::rename(dataset,c("name.y"="country"))
+                                  
+  
+  
+  #for each project, list the country that participated 
+  dataset <-aggregate(dataset$country, 
+                      by=list(dataset$projectReference),
+                      FUN=paste)
+  dataset <- plyr::rename(dataset,c("Group.1"="Project",
+                                    "x" = "ListCountries"))
+
+  # create all the link between two country per project (projA [AA,AB,BC], projB [AA,AB,BC]...) 
+  dataset$ListCountries <- sapply(dataset$ListCountries, func_order)
+  dataset$size <- sapply(dataset$ListCountries, func_size)
+  dataset<- dataset[dataset$size > 1,]  
+  dataset <- subset(dataset, select=-c(size, Project))
+
+  
+  # create all the link between two country
+  dataset$ListCountries <- sapply(dataset$ListCountries, func_createPair)
+  data <- list()
+  for (i in 1:nrow(dataset)) {
+    data[[i]] =func_createFrame(dataset[i,])
+  }
+
+  tmp <- do.call(rbind, data)
+  output <-data.frame(tmp)
+  
+  #aggregate to have: country 1 | country 2 | nb link
+  output<-aggregate(output$X1,
+                    by=list(output$X1, output$X2),
+                    FUN=length)
+  output <-data.frame(output)
+  output <- plyr::rename(output,c("Group.1" = "country1",
+                                  "Group.2" = "country2",
+                                  "x" = "nbLinkTmp"))
+  
+  #add a column for the framework contract 
+  output$frameworkContract <- frameworkContract
+  
+
+  return(output)
+}
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------
+# IV. Build Dataset link ######################################################################################################################
+# ---------------------------------------------------------------------------------------------------------------------------------------------
+
+
+Sys.time()
+FP6_link <- func_BuildLink("FP6", Dataset_FP6Organizations)
+FP6_link$frameworkContract<- "FP6"
+
+Sys.time()
+FP7_link <- func_BuildLink("FP7", Dataset_FP7Organizations)
+FP7_link$frameworkContract<- "FP7"
+
+Sys.time()
+H2020_link <- func_BuildLink("H2020", Dataset_H2020Organizations)
+H2020_link$frameworkContract<- "H2020"
+Sys.time()
+
+Dataset_link <-rbind(rbind(FP6_link, FP7_link), H2020_link)
+
+options(scipen = 10)
+write.table(Dataset_link, "output/NbLink.csv", sep = ";", quote = FALSE)
+options(scipen = 0)
+
