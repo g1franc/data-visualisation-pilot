576ddd57dc6bcb2a4daec59c12818c328e47fde9
brule.daniel@gmail.com <brule.daniel@gmail.com>
2017-01-19 16:34:52 +0100

    adding new dimensions

--- Data cleaning Visualisation 1-V2/Scripts/DataPrep-Gapminder.R
+++ Data cleaning Visualisation 1-V2/Scripts/DataPrep-Gapminder.R
@@ -11,8 +11,10 @@ setwd("C:/Users/bruled/Documents/Pwc Project/2 - Project/DataVisualisation/Visua
 # FP6: https://data.europa.eu/euodp/data/dataset/cordisfp6projects
 # FP7: https://data.europa.eu/euodp/data/dataset/cordisfp7projects
 # H2020: https://data.europa.eu/euodp/data/dataset/cordisH2020projects
-# Population: http://appsso.eurostat.ec.europa.eu/nui/show.do?dataset=demo_pjan&lang=en
-# GDP: http://appsso.eurostat.ec.europa.eu/nui/show.do?dataset=naida_10_gdp&lang=en
+# Population: http://appsso.eurostat.ec.europa.eu/nui/show.do?dataset=demo_pjan&lang=en : demo_pjan_1_Data.csv
+# GDP: http://appsso.eurostat.ec.europa.eu/nui/show.do?dataset=naida_10_gdp&lang=en : naida_10_gdp_1_Data.csv
+# Population by educational attainment level (%) - main indicators (edat_lfse_03): http://appsso.eurostat.ec.europa.eu/nui/show.do?dataset=edat_lfse_03&lang=en - edat_lfse_03_1_Data.csv 
+# Total intramural R&D expenditure (GERD) by sectors of performance: http://appsso.eurostat.ec.europa.eu/nui/show.do?dataset=rd_e_gerdtot&lang=en : rd_e_gerdtot_1_Data.csv
 
 Dataset_FP6Organizations = read.csv("Input/cordis-fp6organizations.csv", header=TRUE, sep=";", stringsAsFactors=FALSE, comment.char="")
 Dataset_FP7Organizations = read.csv("Input/cordis-fp7organizations.csv", header=TRUE, sep=";", stringsAsFactors=FALSE, comment.char="")
@@ -28,6 +30,11 @@ Dataset_Projects <- rbind(rbind(Dataset_FP6Projects, Dataset_FP7Projects), Datas
 Dataset_population  = read.csv("Input/demo_pjan_1_Data.csv", header=TRUE, sep=",", stringsAsFactors=FALSE, comment.char="")
 Dataset_GDP  = read.csv("Input/naida_10_gdp_1_Data.csv", header=TRUE, sep=",", stringsAsFactors=FALSE, comment.char="")
 
+Dataset_PopByEducAttain = read.csv("Input/edat_lfse_03_1_Data.csv", header=TRUE, sep=",", stringsAsFactors=FALSE, comment.char="")
+Dataset_RDExpend = read.csv("Input/rd_e_gerdtot_1_Data.csv", header=TRUE, sep=",", stringsAsFactors=FALSE, comment.char="")
+
+
+
 Dataset_Countries = read.csv("Input/Countries.csv", header=TRUE, sep=";", stringsAsFactors=FALSE, comment.char="")
 #select only EU-28 countries 
 Dataset_Countries <- Dataset_Countries[Dataset_Countries$EU28 == TRUE,]
@@ -39,7 +46,6 @@ Dataset_Countries <- Dataset_Countries[Dataset_Countries$EU28 == TRUE,]
 
 
 ### filter dataset Project 
-
 Dataset_Projects <- merge(Dataset_Projects, Dataset_Countries, by.x=c("coordinatorCountry"), by.y=c("euCode"), all.x=TRUE)
 #select only EU-28 countries
 Dataset_Projects <- Dataset_Projects[Dataset_Projects$EU28 == TRUE,]
@@ -56,9 +62,8 @@ Dataset_Projects$totalCost <- as.numeric(Dataset_Projects$totalCost)
 Dataset_Projects$startDate <- as.Date(as.POSIXlt(as.character(Dataset_Projects$startDate), format="%Y-%m-%d"))
 Dataset_Projects$startDate <- as.numeric(format(Dataset_Projects$startDate, "%Y"))
 
-# remove projects with no start date 
+# remove projects with no start date => ~200 project
 Dataset_Projects <- Dataset_Projects[!is.na(Dataset_Projects$startDate),]
-
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------
 # III. compute number Of Project coordinated per country/year #################################################################################
@@ -150,27 +155,99 @@ output <- subset(output, select=-c(UNIT, NA_ITEM, Flag.and.Footnotes))
 output <- merge(output, Dataset_population, by.x=c("name", "Year"), by.y=c("GEO", "TIME"), all.x=TRUE)
 output <- subset(output, select=-c(AGE, SEX,UNIT, Flag.and.Footnotes))
 
+# ---------------------------------------------------------------------------------------------------------------------------------------------
+# IX. Population by educational attainment level ##############################################################################################
+# ---------------------------------------------------------------------------------------------------------------------------------------------
+Dataset_PopByEducAttain <- subset(Dataset_PopByEducAttain, select=-c(SEX, AGE, UNIT))
+
+Dataset_PopByEducAttainED02 <- Dataset_PopByEducAttain[Dataset_PopByEducAttain$ISCED11 == "ED0-2", ]
+Dataset_PopByEducAttainED02 <- subset(Dataset_PopByEducAttainED02, select=-c(ISCED11))
+Dataset_PopByEducAttainED02 <- plyr::rename(Dataset_PopByEducAttainED02,c("Value" = "% of the population that study up to secondary education"))
+                                
+Dataset_PopByEducAttainED38 <- Dataset_PopByEducAttain[Dataset_PopByEducAttain$ISCED11 == "ED3-8", ]
+Dataset_PopByEducAttainED38 <- subset(Dataset_PopByEducAttainED38, select=-c(ISCED11))
+Dataset_PopByEducAttainED38 <- plyr::rename(Dataset_PopByEducAttainED38,c("Value" = "% of the population that have a secondary or tertiary education"))
+
+Dataset_PopByEducAttainED34 <- Dataset_PopByEducAttain[Dataset_PopByEducAttain$ISCED11 == "ED3_4", ]
+Dataset_PopByEducAttainED34 <- subset(Dataset_PopByEducAttainED34, select=-c(ISCED11))
+Dataset_PopByEducAttainED34 <- plyr::rename(Dataset_PopByEducAttainED34,c("Value" = "% of the population that have a secondary education"))
+
+Dataset_PopByEducAttainED58 <- Dataset_PopByEducAttain[Dataset_PopByEducAttain$ISCED11 == "ED5-8", ]
+Dataset_PopByEducAttainED58 <- subset(Dataset_PopByEducAttainED58, select=-c(ISCED11))
+Dataset_PopByEducAttainED58 <- plyr::rename(Dataset_PopByEducAttainED58,c("Value" = "% of the population that have a tertiary education"))
+
+
+output <- merge(output, Dataset_PopByEducAttainED02, by.x=c("Country", "Year"), by.y=c("GEO", "TIME"), all.x=TRUE)
+output <- merge(output, Dataset_PopByEducAttainED38, by.x=c("Country", "Year"), by.y=c("GEO", "TIME"), all.x=TRUE)
+output <- merge(output, Dataset_PopByEducAttainED34, by.x=c("Country", "Year"), by.y=c("GEO", "TIME"), all.x=TRUE)
+output <- merge(output, Dataset_PopByEducAttainED58, by.x=c("Country", "Year"), by.y=c("GEO", "TIME"), all.x=TRUE)
+
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------
-# IX. compute GDP per capita per country/year #################################################################################################
+# X. Total intramural R&D expenditure #########################################################################################################
+# ---------------------------------------------------------------------------------------------------------------------------------------------
+
+Dataset_RDExpend <- subset(Dataset_RDExpend, select=-c(UNIT))
+Dataset_RDExpend$Value <- sub(' ', '',Dataset_RDExpend$Value)
+Dataset_RDExpend$Value <- as.numeric(Dataset_RDExpend$Value)
+
+
+Dataset_RDExpendAllSect <- Dataset_RDExpend[Dataset_RDExpend$SECTPERF == "All sectors", ]
+Dataset_RDExpendAllSect <- subset(Dataset_RDExpendAllSect, select=-c(SECTPERF))
+Dataset_RDExpendAllSect$Value <- Dataset_RDExpendAllSect$Value * 1000000
+Dataset_RDExpendAllSect <- plyr::rename(Dataset_RDExpendAllSect,c("Value" = "R&D Budget"))
+
+
+Dataset_RDExpendBusiness <- Dataset_RDExpend[Dataset_RDExpend$SECTPERF == "Business enterprise sector", ]
+Dataset_RDExpendBusiness <- subset(Dataset_RDExpendBusiness, select=-c(SECTPERF))
+Dataset_RDExpendBusiness$Value <- Dataset_RDExpendBusiness$Value * 1000000
+Dataset_RDExpendBusiness <- plyr::rename(Dataset_RDExpendBusiness,c("Value" = "R&D Budget of enterprise"))
+
+Dataset_RDExpendGov <- Dataset_RDExpend[Dataset_RDExpend$SECTPERF == "Government sector", ]
+Dataset_RDExpendGov <- subset(Dataset_RDExpendGov, select=-c(SECTPERF))
+Dataset_RDExpendGov$Value <- Dataset_RDExpendGov$Value * 1000000
+Dataset_RDExpendGov <- plyr::rename(Dataset_RDExpendGov,c("Value" = "R&D Budget of government"))
+
+Dataset_RDExpendEduc <- Dataset_RDExpend[Dataset_RDExpend$SECTPERF == "Higher education sector", ]
+Dataset_RDExpendEduc <- subset(Dataset_RDExpendEduc, select=-c(SECTPERF))
+Dataset_RDExpendEduc$Value <- Dataset_RDExpendEduc$Value * 1000000
+Dataset_RDExpendEduc <- plyr::rename(Dataset_RDExpendEduc,c("Value" = "R&D Budget of high education"))
+
+Dataset_RDExpendNonProf <- Dataset_RDExpend[Dataset_RDExpend$SECTPERF == "Private non-profit sector", ]
+Dataset_RDExpendNonProf <- subset(Dataset_RDExpendNonProf, select=-c(SECTPERF))
+Dataset_RDExpendNonProf$Value <- Dataset_RDExpendNonProf$Value * 1000000
+Dataset_RDExpendNonProf <- plyr::rename(Dataset_RDExpendNonProf,c("Value" = "R&D Budget of non-profit organisation"))
+
+
+output <- merge(output, Dataset_RDExpendAllSect, by.x=c("name", "Year"), by.y=c("GEO", "TIME"), all.x=TRUE)
+output <- merge(output, Dataset_RDExpendBusiness, by.x=c("name", "Year"), by.y=c("GEO", "TIME"), all.x=TRUE)
+output <- merge(output, Dataset_RDExpendGov, by.x=c("name", "Year"), by.y=c("GEO", "TIME"), all.x=TRUE)
+output <- merge(output, Dataset_RDExpendEduc, by.x=c("name", "Year"), by.y=c("GEO", "TIME"), all.x=TRUE)
+output <- merge(output, Dataset_RDExpendNonProf, by.x=c("name", "Year"), by.y=c("GEO", "TIME"), all.x=TRUE)
+
+
+# ---------------------------------------------------------------------------------------------------------------------------------------------
+# XI. compute GDP per capita per country/year #################################################################################################
 # ---------------------------------------------------------------------------------------------------------------------------------------------
 output <- plyr::rename(output,c("Value.x"="GDP", "Value.y" = "Population"))
 output$GDP <- sub(',', '',output$GDP)
 output$GDP <- sub(',', '',output$GDP)
 output$GDP <- sub(',', '',output$GDP)
 output$GDP <- as.numeric(output$GDP)
+output$GDP <- output$GDP * 1000000
+
 output$Population <- sub(' ', '',output$Population)
 output$Population <- sub(' ', '',output$Population)
 output$Population <-as.numeric(output$Population)
-output$GDPPerCapita <- output$GDP / output$Population * 1000000
+output$GDPPerCapita <- output$GDP / output$Population
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------
-# X. compute budget managed as coordinators per capita per country/year #######################################################################
+# XII. compute budget managed as coordinators per capita per country/year #####################################################################
 # ---------------------------------------------------------------------------------------------------------------------------------------------
 output$totalBudgetManagedAsCoordinatorsPerCapita <- output$totalBudgetManagedAsCoordinators / output$Population
 
 # ---------------------------------------------------------------------------------------------------------------------------------------------
-# XI. save file ###############################################################################################################################
+# XIII. save file #############################################################################################################################
 # ---------------------------------------------------------------------------------------------------------------------------------------------
 output <- plyr::rename(output,c("Country" = "CountryShort",
                                 "name"="Country", 
@@ -183,8 +260,8 @@ output <- plyr::rename(output,c("Country" = "CountryShort",
 
 output <- output[output$Year != 2017, ]
 output <- output[output$Year != 2018, ]
-
-options(scipen = 4)
+output$GDP <- format(output$GDP, scientific = FALSE)
+options(scipen = 999)
 write.table(output, "output/CountryInformation.csv", sep = "\t", quote = FALSE)
 options(scipen = 0)
 
