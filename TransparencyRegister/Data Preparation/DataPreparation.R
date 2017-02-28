3dddcdb8d6815ac153756b259738633428c58f80
PWCBE\vandeloc <carmen.vandeloo@be.pwc.com>
2017-02-28 17:29:57 +0100

    reordering of columns data prep map

--- TransparencyRegister/Data Preparation/DataPreparation.R
+++ TransparencyRegister/Data Preparation/DataPreparation.R
@@ -86,6 +86,9 @@ for (j in 1:length(listInterest)){
   i<- i+1
 }
 
+#reorder columns
+data <- data[c(1,2,3,5,4)]
+
 #save dataset
 write.table(data, "../Datasets/datasetMap.csv", sep = ";", quote = FALSE, row.names = FALSE)
 
