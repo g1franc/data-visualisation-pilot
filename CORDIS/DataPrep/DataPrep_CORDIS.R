b1a8e0ea8097e1295893f557cf6460679dd3e277
PWCBE\vandeloc <carmen.vandeloo@be.pwc.com>
2017-04-11 10:20:41 +0200

    renaming of files (camelCase)

--- CORDIS/DataPrep/DataPrep_CORDIS.R
+++ CORDIS/DataPrep/DataPrep_CORDIS.R
@@ -8,33 +8,33 @@ arg_4 = "2016"
 
 #### Load arguments ####
 args <- commandArgs(trailingOnly = TRUE)
-arg_1 = args[1] #Chord boolean
-arg_2 = args[2] #Motionchart boolean
-arg_3 = args[3] #Organisations Network boolean
+arg_1 = args[1] #chord boolean
+arg_2 = args[2] #motionchart boolean
+arg_3 = args[3] #organisations Network boolean
 arg_4 = args[4] #update with data until year x
 cat(paste("arguments loaded succesfully:", 
         "\n Chord: ", arg_1, 
-        "\n Motionchart: ", arg_2,
+        "\n Motion chart: ", arg_2,
         "\n Organisations Network: ", arg_3,
         "\n Update with data until year: ", arg_4,
         "\n"))
 
 
 #### Call scripts depending on variables ####
-source("LoadDatasets.R")
+source("dataPrep_loadDatasets.R")
 cat("The required datasets are loaded \n")
 
 if ( arg_1 == "true" ) {
-  source("DataPrep_Chord.R")
+  source("dataPrep_chord.R")
   cat("The data for the chord diagram is now updated \n")
 }
 
 if ( arg_2 == "true" ) {
-  source("DataPrep_Motionchart.R")
+  source("dataPrep_motionChart.R")
   cat("The data for the motionchart is now updated \n")
 }
 
 if ( arg_3 == "true" ) {
-  source("DataPrep_OrganisationsNetwork.R")
+  source("dataPrep_organisationsNetwork.R")
   cat("The data for the organisations network is now updated \n")
 }
--- CORDIS/DataPrep/DataPrep_Motionchart.R
+++ CORDIS/DataPrep/DataPrep_Motionchart.R
@@ -276,53 +276,53 @@ output <- output[output$Year <= arg_4, ]
 output$GDP <- format(output$GDP, scientific = FALSE)
 
 options(scipen = 999)
-write.table(output, "../Datasets/Motionchart.csv", sep = ",", quote = FALSE, row.names = FALSE)
+write.table(output, "../Datasets/motionChart.csv", sep = ",", quote = FALSE, row.names = FALSE)
 options(scipen = 0)
 
-
-
+# ---------------------------------------------------------------------------------------------------------------------------------------------
+# XIII. convert to correct js format and save file ############################################################################################
+# ---------------------------------------------------------------------------------------------------------------------------------------------
 
 writeColNames <- function (data) {
-  cat('[', file="../Datasets/Motionchart.js", append = TRUE)
+  cat('[', file="../Datasets/motionChart.js", append = TRUE)
   for (i in 1:(length(data)-1)) {
-    cat('"', file="../Datasets/Motionchart.js", append = TRUE)
-    cat(colnames(data)[i], file="../Datasets/Motionchart.js", append = TRUE)
-    cat('", ', file="../Datasets/Motionchart.js", append = TRUE)
+    cat('"', file="../Datasets/motionChart.js", append = TRUE)
+    cat(colnames(data)[i], file="../Datasets/motionChart.js", append = TRUE)
+    cat('", ', file="../Datasets/motionChart.js", append = TRUE)
   }
-  cat('"', file="../Datasets/Motionchart.js", append = TRUE)
-  cat(colnames(data)[length(data)], file="../Datasets/Motionchart.js", append = TRUE)
-  cat('"], \n', file="../Datasets/Motionchart.js", append = TRUE)
+  cat('"', file="../Datasets/motionChart.js", append = TRUE)
+  cat(colnames(data)[length(data)], file="../Datasets/motionChart.js", append = TRUE)
+  cat('"], \n', file="../Datasets/motionChart.js", append = TRUE)
 }
 
 writeData <- function (data) {
   for (j in 1:(nrow(data)-1)) {
   
-      cat("[", file="../Datasets/Motionchart.js", append = TRUE)
+      cat("[", file="../Datasets/motionChart.js", append = TRUE)
       for (i in 1:(length(data)-1)) {
-        cat('"', file="../Datasets/Motionchart.js", append = TRUE)
-        cat(data[j,i], file="../Datasets/Motionchart.js", append = TRUE)
-        cat('", ', file="../Datasets/Motionchart.js", append = TRUE)
+        cat('"', file="../Datasets/motionChart.js", append = TRUE)
+        cat(data[j,i], file="../Datasets/motionChart.js", append = TRUE)
+        cat('", ', file="../Datasets/motionChart.js", append = TRUE)
       }
-      cat('"', file="../Datasets/Motionchart.js", append = TRUE)
-      cat(data[j,length(data)], file="../Datasets/Motionchart.js", append = TRUE)
-      cat('"], \n', file="../Datasets/Motionchart.js", append = TRUE)
+      cat('"', file="../Datasets/motionChart.js", append = TRUE)
+      cat(data[j,length(data)], file="../Datasets/motionChart.js", append = TRUE)
+      cat('"], \n', file="../Datasets/motionChart.js", append = TRUE)
       
   }
   
-  cat("[", file="../Datasets/Motionchart.js", append = TRUE)
+  cat("[", file="../Datasets/motionChart.js", append = TRUE)
   for (i in 1:(length(data)-1)) {
-    cat('"', file="../Datasets/Motionchart.js", append = TRUE)
-    cat(data[j,i], file="../Datasets/Motionchart.js", append = TRUE)
-    cat('", ', file="../Datasets/Motionchart.js", append = TRUE)
+    cat('"', file="../Datasets/motionChart.js", append = TRUE)
+    cat(data[j,i], file="../Datasets/motionChart.js", append = TRUE)
+    cat('", ', file="../Datasets/motionChart.js", append = TRUE)
   }
-  cat('"', file="../Datasets/Motionchart.js", append = TRUE)
-  cat(data[j,length(data)], file="../Datasets/Motionchart.js", append = TRUE)
-  cat('"] \n', file="../Datasets/Motionchart.js", append = TRUE)
+  cat('"', file="../Datasets/motionChart.js", append = TRUE)
+  cat(data[j,length(data)], file="../Datasets/motionChart.js", append = TRUE)
+  cat('"] \n', file="../Datasets/motionChart.js", append = TRUE)
   
-  cat('];', file="../Datasets/Motionchart.js", append = TRUE)
+  cat('];', file="../Datasets/motionChart.js", append = TRUE)
 }
 
-cat('var data = [ \n', file = "../Datasets/Motionchart.js")
+cat('var data = [ \n', file = "../Datasets/motionChart.js")
 writeColNames(output)
 writeData(output)
-
